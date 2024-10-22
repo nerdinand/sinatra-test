269eec4fd7bcab1fcc9109f1148b7ec089110bbc
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2024-10-22 22:27:22 +0200

    Make app name configurable

--- app.rb
+++ app.rb
@@ -2,10 +2,12 @@
 
 require 'sinatra'
 
+APP_NAME = ENV['APP_NAME']
+
 get '/up' do
   status(200)
 end
 
 get '/' do
-  'It works!'
+  "Sinatra app deployed as <b>#{APP_NAME}</b> reached under <b>#{request.host}</b>"
 end
